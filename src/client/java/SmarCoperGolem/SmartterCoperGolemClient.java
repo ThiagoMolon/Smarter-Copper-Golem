959135a2fe9099746fb0c6f0647e3516dff02c7f
Thiago-Molon <thiago.molon@bellosoft.com>
2026-01-07 19:09:13 -0300

    base

---
+++ src/client/java/SmarCoperGolem/SmartterCoperGolemClient.java
@@ -0,0 +1,10 @@
+package SmarCoperGolem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmartterCoperGolemClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
